67985cb3725a4ca7ae93076106c2a0a601a6227a
Pedro <juniorsj33@gmail.com>
2019-05-29 11:29:14 -0300

    Upando meu primeiro projeto

---
+++ src/main/java/com/ufc/br/controller/PessoaController.java
@@ -0,0 +1,61 @@
+package com.ufc.br.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ufc.br.model.Pessoa;
+import com.ufc.br.repository.PessoaRepository;
+import com.ufc.br.service.PessoaService;
+
+
+@Controller
+@RequestMapping("/pessoa")
+public class PessoaController {
+	
+	@Autowired
+	private PessoaService pessoaService;
+	
+	@RequestMapping("/formulario")
+	public ModelAndView form() {
+		ModelAndView mv = new ModelAndView("Formulario");
+		mv.addObject("pessoa", new Pessoa());
+		return mv;
+	}
+	
+	@PostMapping("/salvar")
+	public ModelAndView salvar(Pessoa pessoa) {
+		pessoaService.cadastrar(pessoa);
+		ModelAndView mv = new ModelAndView("redirect:/pessoa/listar");
+		return mv;
+}
+	@GetMapping("/listar")
+	public ModelAndView listar() {
+		List<Pessoa> pessoas = pessoaService.listarTodos();
+		ModelAndView mv = new ModelAndView("PaginaListagem");
+		mv.addObject("listaPessoas",pessoas);
+		
+		return mv;
+	}
+	
+	@RequestMapping("/excluir/{codigo}")
+	public ModelAndView deletar(@PathVariable Long codigo) {
+		pessoaService.excluirPessoa(codigo);
+		ModelAndView mv = new ModelAndView("redirect:/pessoa/listar");
+		return mv;
+	}
+	
+	@RequestMapping("/atualizar/{codigo}")
+	public ModelAndView atualizarPessoa(@PathVariable long codigo) {
+		Pessoa pessoa = pessoaService.buscarporId(codigo);
+		ModelAndView mv = new ModelAndView("Formulario");
+		mv.addObject("pessoa", pessoa);
+		return mv;
+	}
+}
